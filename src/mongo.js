193da89920fe680ad63141df56d80fe441d7841b
Gustavo Velazquez <gustavo.velazquezm@outlook.com>
2021-01-12 21:30:22 -0600

    try-catch in mongo connection

--- src/mongo.js
+++ src/mongo.js
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
 
 export async function setupMongo() {
-  await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-  console.log("Mongo is ready");
+    console.log("Mongo is ready");
+  } catch (error) {
+    console.log("Mongo error: ", error);
+  }
 }
